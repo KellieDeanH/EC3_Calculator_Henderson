ae2c40c14b9240bdd4a408f0c4a413b144dd2ae6
Kellie H <kellie.henderson@my.tccd.edu>
2019-12-02 00:48:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sstring >
+#include <algorithm>
+using namespace std;
+
+double calculator(double,double, string);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() 
+{
+
+  
+
+}
